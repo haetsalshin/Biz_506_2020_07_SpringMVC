26ee45c16d6d2a64623b99b8c869877ede5922ce
haetsalshin <sinsin0902@daum.net>
2020-10-19 16:46:07 +0900

    1019 BBS Project

---
+++ SpMVC_BBS_2020/src/main/java/com/biz/bbs/model/BBsVO.java
@@ -0,0 +1,29 @@
+package com.biz.bbs.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class BBsVO {
+	
+	
+	private long b_seq;
+	private String b_date;
+	private String b_time;
+	private String b_writer;
+	private String b_subject;
+	private String b_content;
+	private int b_count;
+	private String b_file;
+
+
+}
